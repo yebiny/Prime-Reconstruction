48a08cd9528aa6c0b75022a68eeeb6a56a92863b
yebiny <yebinyoun@gmail.com>
2021-01-26 19:17:09 +0900

    scripts for new subj

---
+++ fmri/add_subj.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SUBJ=${1}
+
+check=data/$SUBJ
+sample=data/subSample
+
+
+if [ -d "$check" ]; then
+  read -t 5 -p "directory is already exist. " exit;
+else 
+cp -rf $sample $check
+fi
