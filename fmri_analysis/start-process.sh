6c61898d20cc10eb1c428778201cdf650ab19d95
yebiny <yebinyoun@gmail.com>
2021-02-18 18:06:53 +0900

    change same val

--- fmri_analysis/start-process.sh
+++ fmri_analysis/start-process.sh
@@ -2,8 +2,8 @@
 source path.sh
 
 SUBJ=${1}
-DATA=${2}
+RAWDATA=${2}
 
-bash scripts/convert-raw.sh $SUBJ $DATA $RESULTS/$SUBJ/1-convert
+bash scripts/convert-raw.sh $SUBJ $RAWDATA $RESULTS/$SUBJ/1-convert
 bash scripts/reorient-las.sh $RESULTS/$SUBJ/1-convert/ $RESULTS/$SUBJ/2-reorient
 bash scripts/bet.sh $RESULTS/$SUBJ/2-reorient/ $RESULTS/$SUBJ/3-bet/
