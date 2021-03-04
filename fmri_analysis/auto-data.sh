108205e3f0f37c8a9736fc2e396b1781ac605af6
yebiny <yebinyoun@gmail.com>
2021-03-04 15:51:35 +0900

    add files

---
+++ fmri_analysis/auto-data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source path.sh
+
+sublist='sub02 sub04 sub05 sub06 sub07 sub08 sub09 sub10 sub11 sub13 sub14 sub15 sub16 sub17 sub18 sub19 sub21 sub22 sub23 sub24'
+
+for sub in $sublist
+
+do
+bash start-data-generator.sh $sub
+done
